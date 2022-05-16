6e2f1613877bd70749387ddcda57489f8881e1e4
grabmairarthur <arthur.g@gmx.at>
2022-05-16 16:51:38 +0200

    Arthuro hat Recht

--- grabmairarthur_thow21_android/ToolsProject/src/Dummy1.java
+++ grabmairarthur_thow21_android/ToolsProject/src/Dummy1.java
@@ -1,14 +1,17 @@
+import java.util.Iterator;
 
 public class Dummy1 {
-		
+
 	void init() {
-		
+
 		while (true) {
 			System.out.println("Projekttools sind sowas von toll!");
 		}
-		
+
 	}
 
-	public void thisIsANewMethod(){
-}
+	public void thisIsANewMethod() {
+		int x = 0;
+		x = 567;
+	}
 }
